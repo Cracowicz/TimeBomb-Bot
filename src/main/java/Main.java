c7d7d83f6107fb96e740d6e310dd32b8c202203e
DeLaBlancheur <migueldubie76@hotmail.fr>
2020-04-18 17:00:43 +0200

    Add bot activity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
@@ -9,6 +10,7 @@ public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = JDABuilder.createDefault(System.getenv("TimeBombToken"));
         builder.addEventListeners(new EventListener());
+        builder.setActivity(Activity.playing("!help"));
         builder.build();
     }
 }
